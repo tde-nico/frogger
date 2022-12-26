303faaa1a41d2c9fb4475f0c178c2351afe44163
tde-nico <101901057+tde-nico@users.noreply.github.com>
2022-12-26 09:12:15 +0100

    init

---
+++ code/main.py
@@ -0,0 +1,103 @@
+import pygame, sys 
+from settings import *
+from player import Player
+from car import Car
+from random import choice, randint
+from sprite import SimpleSprite, LongSprite
+
+class AllSprites(pygame.sprite.Group):
+	def __init__(self):
+		super().__init__()
+		self.offset = pygame.math.Vector2()
+		self.bg = pygame.image.load('../graphics/main/map.png').convert()
+		self.fg = pygame.image.load('../graphics/main/overlay.png').convert_alpha()
+
+	def customize_draw(self):
+
+		# change the offset vector
+		self.offset.x = player.rect.centerx - WINDOW_WIDTH / 2
+		self.offset.y = player.rect.centery - WINDOW_HEIGHT / 2
+
+		# blit the bg
+		display_surface.blit(self.bg,-self.offset)
+
+		for sprite in sorted(self.sprites(),key = lambda sprite: sprite.rect.centery):
+			offset_pos = sprite.rect.topleft - self.offset
+			display_surface.blit(sprite.image, offset_pos)
+
+		display_surface.blit(self.fg,-self.offset)
+		
+# basic setup
+pygame.init()
+display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+pygame.display.set_caption('Frogger')
+clock = pygame.time.Clock()
+
+# groups 
+all_sprites = AllSprites()
+obstacle_sprites = pygame.sprite.Group()
+
+# sprites 
+player = Player((2062,3274),all_sprites,obstacle_sprites)
+
+# timer 
+car_timer = pygame.event.custom_type()
+pygame.time.set_timer(car_timer, 50)
+pos_list = []
+
+# font 
+font = pygame.font.Font(None, 50)
+text_surf = font.render('You won!',True,'White')
+text_rect = text_surf.get_rect(center = (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
+
+# music
+music = pygame.mixer.Sound('../audio/music.mp3')
+music.play(loops = -1)
+
+# sprite setup
+for file_name, pos_list in SIMPLE_OBJECTS.items():
+	path = f'../graphics/objects/simple/{file_name}.png'
+	surf = pygame.image.load(path).convert_alpha()
+	for pos in pos_list:
+		SimpleSprite(surf,pos,[all_sprites,obstacle_sprites])
+
+for file_name, pos_list in LONG_OBJECTS.items():
+	surf = pygame.image.load(f'../graphics/objects/long/{file_name}.png').convert_alpha()
+	for pos in pos_list:
+		LongSprite(surf,pos,[all_sprites,obstacle_sprites])
+
+# game loop
+while True:
+	
+	# event loop 
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			pygame.quit()
+			sys.exit()
+		if event.type == car_timer:
+			random_pos = choice(CAR_START_POSITIONS)
+			if random_pos not in pos_list:
+				pos_list.append(random_pos)
+				pos = (random_pos[0],random_pos[1] + randint(-8,8))
+				Car(pos,[all_sprites,obstacle_sprites])
+			if len(pos_list) > 5:
+				del pos_list[0]
+
+	# delta time 
+	dt = clock.tick() / 1000
+
+	# draw a bg
+	display_surface.fill('black')
+
+	if player.pos.y >= 1180:
+		# update 
+		all_sprites.update(dt)
+
+		# draw
+		all_sprites.customize_draw()
+	else:
+		display_surface.fill('teal')
+		display_surface.blit(text_surf,text_rect)
+
+	# update the display surface
+	pygame.display.update()
---
+++ code/player.py
@@ -0,0 +1,140 @@
+import pygame, sys
+from os import walk
+
+class Player(pygame.sprite.Sprite):
+	def __init__(self, pos, groups, collision_sprites):
+		super().__init__(groups)
+		
+		# image 
+		self.import_assets()
+		self.frame_index = 0
+		self.status = 'up'
+		# self.image = self.animation[self.frame_index]
+		self.image = self.animations[self.status][self.frame_index]
+		self.rect = self.image.get_rect(center = pos)
+
+		# float based movement 
+		self.pos = pygame.math.Vector2(self.rect.center)
+		self.direction = pygame.math.Vector2()
+		self.speed = 200
+
+		# collisions
+		self.collision_sprites = collision_sprites
+		self.hitbox = self.rect.inflate(0,-self.rect.height / 2)
+
+	def collision(self,direction):
+		if direction == 'horizontal':
+			for sprite in self.collision_sprites.sprites():
+				if sprite.hitbox.colliderect(self.hitbox):
+					if hasattr(sprite,'name') and sprite.name == 'car':
+						pygame.quit()
+						sys.exit()
+
+					if self.direction.x > 0: # moving right
+						self.hitbox.right = sprite.hitbox.left
+						self.rect.centerx = self.hitbox.centerx
+						self.pos.x = self.hitbox.centerx
+					if self.direction.x < 0: # moving left
+						self.hitbox.left = sprite.hitbox.right
+						self.rect.centerx = self.hitbox.centerx
+						self.pos.x = self.hitbox.centerx
+		else:
+			for sprite in self.collision_sprites.sprites():
+				if sprite.hitbox.colliderect(self.hitbox):
+					if hasattr(sprite,'name') and sprite.name == 'car':
+						pygame.quit()
+						sys.exit()
+
+					if self.direction.y > 0: # moving down
+						self.hitbox.bottom = sprite.hitbox.top
+						self.rect.centery = self.hitbox.centery
+						self.pos.y = self.hitbox.centery
+					if self.direction.y < 0: # moving up
+						self.hitbox.top = sprite.hitbox.bottom
+						self.rect.centery = self.hitbox.centery
+						self.pos.y = self.hitbox.centery
+
+	def import_assets(self): 
+		self.animations = {}
+		for index, folder in enumerate(walk('../graphics/player')):
+			if index == 0:
+				for name in folder[1]:
+					self.animations[name] = []
+			else:
+				for file_name in folder[2]:
+					path = folder[0].replace('\\', '/') + '/' + file_name # exercise: turn \ -> /
+					surf = pygame.image.load(path).convert_alpha()
+					key = folder[0].split('\\')[1]
+					self.animations[key].append(surf)
+
+	def move(self,dt):
+
+		# normalize a vector -> the length of a vector is going to be 1
+		if self.direction.magnitude() != 0:
+			self.direction = self.direction.normalize()
+
+		# horizontal movement + collision
+		self.pos.x += self.direction.x * self.speed * dt
+		self.hitbox.centerx = round(self.pos.x)
+		self.rect.centerx = self.hitbox.centerx
+		self.collision('horizontal')
+
+		# vertical movement + collision
+		self.pos.y += self.direction.y * self.speed * dt
+		self.hitbox.centery = round(self.pos.y)
+		self.rect.centery = self.hitbox.centery
+		self.collision('vertical')
+
+	def input(self):
+		keys = pygame.key.get_pressed()
+		
+		# horizontal input 
+		if keys[pygame.K_RIGHT]:
+			self.direction.x = 1
+			self.status = 'right'
+		elif keys[pygame.K_LEFT]:
+			self.direction.x = -1
+			self.status = 'left'
+		else:
+			self.direction.x = 0
+
+		# vertical input 
+		if keys[pygame.K_UP]:
+			self.direction.y = -1
+			self.status = 'up'
+		elif keys[pygame.K_DOWN]:
+			self.direction.y = 1
+			self.status = 'down'
+		else:
+			self.direction.y = 0
+
+	def animate(self,dt):
+		current_animation = self.animations[self.status]
+		
+		if self.direction.magnitude() != 0:
+			self.frame_index += 10 * dt
+			if self.frame_index >= len(current_animation):
+				self.frame_index = 0
+		else:
+			self.frame_index = 0
+		self.image = current_animation[int(self.frame_index)]
+
+	def restrict(self):
+		if self.rect.left < 640:
+			self.pos.x = 640 + self.rect.width / 2
+			self.hitbox.left = 640
+			self.rect.left = 640
+		if self.rect.right > 2560:
+			self.pos.x = 2560 - self.rect.width / 2
+			self.hitbox.right = 2560
+			self.rect.right = 2560
+		if self.rect.bottom > 3500:
+			self.pos.y = 3500 - self.rect.height / 2
+			self.rect.bottom = 3500
+			self.hitbox.centery = self.rect.centery
+
+	def update(self,dt):
+		self.input()
+		self.move(dt)
+		self.animate(dt)
+		self.restrict()
